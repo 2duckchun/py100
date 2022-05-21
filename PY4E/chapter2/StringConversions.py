84bf2d3a487e6e84c2d3b09cff97aa3b7a92bf49
2DC <kkts9308@gmail.com>
2022-05-21 23:27:55 +0900

    chapter2

---
+++ PY4E/chapter2/StringConversions.py
@@ -0,0 +1,15 @@
+sval = '123';
+type(sval);
+print(sval + 1);
+# 에러
+
+
+ival = int(sval);
+type(ival);
+print(ival + 1);
+# 출력
+
+
+nsv = 'hello bob';
+niv = int(nsv);
+# 에러
---
+++ PY4E/chapter2/usfloor.py
@@ -0,0 +1,6 @@
+inp = input('Europe floor');
+# input 함수는 문자열을 반환한다.
+# 그래서 integer로 바꿔줘야 한다.
+
+usf = int(inp) + 1;
+print('US Floor', usf)
