cdee48be3f43231873768afcbea888dea2d8a422
2DC <kkts9308@gmail.com>
2022-05-24 19:18:16 +0900

    study

---
+++ PY4E/chapter5/finding.py
@@ -0,0 +1,8 @@
+Smallist_so_far = 9
+print('Before', Smallist_so_far)
+for the_num in [9, 41, 12, 3, 74, 15] :
+    if the_num < Smallist_so_far :
+        Smallist_so_far = the_num
+    print(Smallist_so_far, the_num)
+
+print('After', Smallist_so_far)
