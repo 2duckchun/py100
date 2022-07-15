9178d4174dcade3e106d2f070166e6aa519790e7
2DC <kkts9308@gmail.com>
2022-07-15 23:36:17 +0900

    py
    정처기 공부

--- practice.py
+++ practice.py
@@ -1,10 +1,5 @@
-# range(), step 실습
-
-n = int(input('정수를 입력하세요: '))
-
-sum = 0
-
-for i in range(1, n, 2):
-    sum += i
-
-print(f'1부터 {n}까지의 정수의 합 : {sum}')
+class CharClass:
+    a={'seoul', 'kyeongi', 'Incheon', 'daejeon', 'daegu', 'pusan'};
+myVar = CharClass();
+str01 = '';
+print(myVar.a);
