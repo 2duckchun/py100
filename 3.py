a6de5b43a9cc8e6d7dc9e18ff98e2565717f67f5
2DC <kkts9308@gmail.com>
2022-07-16 21:25:59 +0900

    Update 3.py
    정처기 파이썬 문제

--- 3.py
+++ 3.py
@@ -1,2 +1,7 @@
-for i in range(10, 0, -2):
-    print(i)
+class Charclass:
+    a = ['Seoul', 'Kyeongi', 'Inchon', 'Daejeon', 'Daegu', 'Pusan'];
+myVar = Charclass()
+str01 = ''
+for i in myVar.a:
+    str01 = str01 + i[0]
+print(str01)
